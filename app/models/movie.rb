49296e2f25f794f4633f6e6b03cb4a527fe5e5bb
Gosia <malgorzata.kska@gmail.com>
2021-06-22 20:44:43 +0200

    Add released method to Movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,7 @@ class Movie < ApplicationRecord
     self.total_gross.blank? || self.total_gross < 225000000
   end
 
+  def self.released
+    where("released_on < ?", Time.now ).order(released_on: :desc)
+  end
 end
