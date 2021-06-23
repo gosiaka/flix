cca74b8a0a3f167a15928b3c418ecdc09dc1a2ca
Gosia <malgorzata.kska@gmail.com>
2021-06-23 18:33:33 +0200

    Update Movie actions: update & create

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,9 +13,12 @@ class MoviesController < ApplicationController
 
   def update    
     @movie = Movie.find(params[:id])
-    @movie.update(movie_params)
-
-    redirect_to movie_path
+    
+    if @movie.update(movie_params)
+      redirect_to movie_path
+    else
+      render :edit
+    end   
   end
 
   def new
@@ -23,8 +26,12 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.create(movie_params)
-    redirect_to @movie
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to @movie
+    else
+      render :new
+    end   
   end
 
   def destroy
