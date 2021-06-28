de2863d40b42befe65ff424a3e3dc3be2ee27fb2
Gosia <malgorzata.kska@gmail.com>
2021-06-28 13:02:04 +0200

    Add Destroy Review action to  index page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -20,6 +20,12 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @review = @movie.reviews.find(params[:id])
+    @review.destroy
+    redirect_to movie_reviews_path, alert: "Review removed successfully"
+  end
+
   private
 
   def review_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root "movies#index"
   
   resources :movies do
-    resources :reviews, only: [:index, :new, :create]
+    resources :reviews, only: [:index, :new, :create, :destroy, :edit, :update]
   end
 end
