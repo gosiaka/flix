e7fe1b060cfa48192488275e8c1acbb9d868b8b2
Gosia <malgorzata.kska@gmail.com>
2021-06-30 17:46:47 +0200

    Add Users Index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root "movies#index"
 
-  resources :users
+  resources :users, only: [:index, :show]
   
   resources :movies do
     resources :reviews, only: [:index, :new, :create, :destroy, :edit, :update]
