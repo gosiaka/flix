e7b2039700370f7eaefe08fa0d5a6c8a8f5fd891
Gosia <malgorzata.kska@gmail.com>
2021-07-04 21:10:02 +0200

    Create SignUp page for Users (new & create actions)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   root "movies#index"
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :create]
+  get "signup" => "users#new"
   
   resources :movies do
     resources :reviews, only: [:index, :new, :create, :destroy, :edit, :update]
