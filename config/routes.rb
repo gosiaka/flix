7582f26d82336ffc08700b11983e2141471b39ce
Gosia <malgorzata.kska@gmail.com>
2021-07-05 12:07:41 +0200

    Add Delete feature to User Account

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root "movies#index"
 
-  resources :users, only: [:index, :show, :create, :edit, :update]
+  resources :users, only: [:index, :show, :create, :edit, :update, :destroy]
   get "signup" => "users#new"
   
   resources :movies do
