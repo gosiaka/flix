1762f14e0130dfc1c2f8209b542b9325b5ae2b45
Gosia <malgorzata.kska@gmail.com>
2021-07-09 14:18:50 +0200

    Add Sessions controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show, :create, :edit, :update, :destroy]
   get "signup" => "users#new"
+
+  resource :session, only: [:new, :create, :destroy] # singular resource
   
   resources :movies do
     resources :reviews, only: [:index, :new, :create, :destroy, :edit, :update]
