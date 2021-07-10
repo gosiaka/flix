3fcbb7ab03f49c2846329dd2ede96e47c05c18ff
Gosia <malgorzata.kska@gmail.com>
2021-07-10 11:31:39 +0200

    Add Create Session feature by email/username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email_or_username]) || User.find_by(username: params[:email_or_username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user, notice: "Nice to see you again #{user.username}"
+    else
+      flash.now[:alert] = "Invalid email/password combitanion!"
+      render :new
+    end
   end
 
   def destroy
