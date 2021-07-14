5385eaa098f6c8d8b07e1e98970b22f16f7264bc
Gosia <malgorzata.kska@gmail.com>
2021-07-14 14:45:43 +0200

    Update application_helper current_user to work faster

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
 
   def current_user
-    User.find(session[:user_id]) if session[:user_id]
+    # dodajemy zmienną @current_user, żeby app nie musiała za każdym razem szukać user_id.zmienna zostaje zapisana i jest używana podczas jednego zapytania
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
