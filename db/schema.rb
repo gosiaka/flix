7804989c3528f3afce66836ae85d5f997c5b8a98
Gosia <malgorzata.kska@gmail.com>
2021-07-16 17:26:36 +0200

    Add Admin column to User Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_05_101219) do
+ActiveRecord::Schema.define(version: 2021_07_16_152352) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2021_07_05_101219) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "reviews", "movies"
