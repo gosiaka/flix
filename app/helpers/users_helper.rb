ae6094d83433a663542b99396d66b2a7a625f094
Gosia <malgorzata.kska@gmail.com>
2021-07-22 23:18:16 +0200

    Add users helper and refactor Movies & Users Views

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,8 @@ module UsersHelper
     url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}?s=#{size}"
     image_tag(url, alt: user.name)
   end
+
+  def name_or_username(user)
+    user.username || user.name
+  end
 end
